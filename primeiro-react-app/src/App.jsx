5b14606f788c6163c518b92e97dd736b6ee0bd86
Matheus M. de Santana <matheus.m.santana87@gmail.com>
2022-07-22 15:54:47 -0300

    primeiros passos em react

---
+++ primeiro-react-app/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Button from "./components/button";
+
+const App = () =>{
+  return(
+    <div>
+      <h1>Hello world</h1>
+      <p>esse é meu primeiro site usando React</p>
+      <Button>clique em mim</Button>
+    </div>
+  )
+}
+
+export default App
---
+++ primeiro-react-app/src/components/button.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+import './button.css'
+
+const Button = () =>{
+    <button className="bt">
+    </button>
+}
+
+export default Button
