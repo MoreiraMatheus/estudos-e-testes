60e7b8ba9cfb7e2de21eb107cd09defa75f971b3
Matheus M. de Santana <matheus.m.santana87@gmail.com>
2022-09-07 10:09:32 -0300

    finalizando script

--- api/JsonPlaceholder/teste1.js
+++ api/JsonPlaceholder/teste1.js
@@ -1,18 +1,32 @@
 const baseUrl = 'https://jsonplaceholder.typicode.com/'
 
-const titulo = document.getElementById('titulo')
-const conteudo = document.getElementById('conteudo')
-const user = document.getElementById('user')
-
 async function buscaPosts(){
-  const posts = await fetch(baseUrl + 'posts/1')
+  const posts = await fetch(baseUrl + 'posts')
   const postsTratados = await posts.json()
   return postsTratados
 }
 
+function createPost(titulo, conteudo, idUsuario){
+  const div = document.createElement('div')
+  const h1 = document.createElement('h1')
+  const p = document.createElement('p')
+  const span = document.createElement('span')
+
+  h1.innerText = titulo
+  p.innerText = conteudo
+  span.innerText = 'ID Usuário: '+ idUsuario
+
+  div.appendChild(h1)
+  div.appendChild(p)
+  div.appendChild(span)
+
+  return div
+}
+
 buscaPosts()
-  .then(({userId, title, body}) => {
-    titulo.innerText = title
-    conteudo.innerText = body
-    user.innerText += userId
+  .then(response => {
+    for(let i = 0; i < response.length; i++){
+      const post = response[i]
+      document.body.appendChild(createPost(post.title, post.body, post.userId))
+    }
   })
