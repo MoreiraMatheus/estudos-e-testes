50f0529fa9ba70f7349c319c9cdb9f947ec212e6
Matheus M. de Santana <matheus.m.santana87@gmail.com>
2022-09-08 14:57:44 -0300

    testanod try catch

---
+++ tratamento de erro/teste.js
@@ -0,0 +1,12 @@
+//bloco de código com potencial de gerar um erro
+try {
+  throw new Error('nada para mostrar aqui')
+}
+//bloco de código que será rodado caso o bloco anterior gere um erro
+catch (error) {
+  console.log('erro encontrado:\n' , error.message)
+}
+//bloco de código que será excecutado independe se houver erro ou não
+finally{
+  console.log('essa mensagem sempre será exibida :)')
+}
