f564b88cc235f5bfddaef04c2170a2a79ea4b437
Matheus M. de Santana <matheus.m.santana87@gmail.com>
2022-12-12 18:47:46 -0300

    Limpando ambiente

--- playground react/react-playground/src/Components/Button/styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const Button = styled.button`
-  background-color: ${({ theme }) => theme.color};
-  color: ${({theme}) => theme.text};
-  cursor: pointer;
-  border: none;
-  padding: 4px;
-`
