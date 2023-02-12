d3d76768d60af5195f1771d95204467f3c140c35
Matheus M. de Santana <matheus.m.santana87@gmail.com>
2023-02-12 16:10:39 -0300

    criando interação básica com o form

--- formulario/validate/script.js
+++ formulario/validate/script.js
@@ -1,1 +1,10 @@
 const form = document.querySelector('form')
+const nome = document.querySelector('#iName')
+const age = document.querySelector('#iAge')
+const email = document.querySelector('#iEmail')
+const job = document.querySelector('#iJob')
+const message = document.querySelector('#iMessage')
+
+function submitForm(){
+  console.log('test')
+}
