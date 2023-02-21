d7cc6ed94b0afa60ec33e29c80ebab6a12fa25d1
Matheus M. de Santana <matheus.m.santana87@gmail.com>
2023-02-21 08:31:20 -0300

    formulário montado

--- formulario/validate/script.js
+++ formulario/validate/script.js
@@ -1,10 +1,21 @@
 const form = document.querySelector('form')
-const nome = document.querySelector('#iName')
+const fullName = document.querySelector('#iName')
 const age = document.querySelector('#iAge')
 const email = document.querySelector('#iEmail')
 const job = document.querySelector('#iJob')
 const message = document.querySelector('#iMessage')
 
 function submitForm(){
-  console.log('test')
+  console.log(`fullName: ${fullName.value}`)
+  console.log(`age: ${age.value}`)
+  console.log(`email: ${email.value}`)
+  console.log(`job: ${job.value}`)
+  console.log(`message: ${message.value}`)
+
+  window.alert(`os dados enviados foram:
+  fullName: ${fullName.value}
+  age: ${age.value}
+  email: ${email.value}
+  job: ${job.value}
+  message: ${message.value}`)
 }
