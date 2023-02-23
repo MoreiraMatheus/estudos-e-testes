34eab19181547f3ac2ed9a6960a9101fe4aa26db
Matheus M. de Santana <matheus.m.santana87@gmail.com>
2023-02-23 20:57:51 -0300

    criando pequena validação de dados

--- formulario/validate/script.js
+++ formulario/validate/script.js
@@ -8,14 +8,34 @@ const popup = document.querySelector('dialog')
 const closePopup = document.querySelector('#closePopup')
 const dataP = document.querySelector('#data')
 
+let jobDescription = ''
+
 function submitForm(){
   popup.showModal()
   dataP.innerHTML = ''
   dataP.innerHTML += `fullName: ${fullName.value}</br>`
   dataP.innerHTML += `age: ${age.value}</br>`
   dataP.innerHTML += `email: ${email.value}</br>`
-  dataP.innerHTML += `job: ${job.value}</br>`
-  dataP.innerHTML += `message: ${message.value}</br>`
+
+  switch(Number(job.value)){
+    case 0:
+      jobDescription = 'profissão não informada'
+    break
+    case 1:
+      jobDescription = 'Front-end'
+    break
+    case 2:
+      jobDescription = 'Back-end'
+    break
+    case 3:
+      jobDescription = 'FullStack'
+    break
+  }
+  dataP.innerHTML += `job: ${jobDescription}</br>`
+
+  if(message.value !== ''){
+    dataP.innerHTML += `message: ${message.value}</br>`
+  }
 }
 
 closePopup.onclick = () => {
